254aff73ed427fad5517fe477788a67fdcdbe33a
Asad45AsRa <143110934+Asad45AsRa@users.noreply.github.com>
2023-09-24 00:36:36 +0500

    web project

--- project/src/Components/Navbar.jsx
+++ project/src/Components/Navbar.jsx
@@ -3,14 +3,15 @@ import 'F:/coding/Project/project/src/index.css'
 
 function Navbar() {
   return (
-    <nav className='flex my-2 py-1 bg-slate-600 text-white font-medium justify-between'>
+    <nav className='shadow-md flex my-2 py-1 bg-slate-600 text-white font-medium justify-between font-[Poppins]
+    md:px-10 items-center'>
       <span><img src="https://www.logodee.com/wp-content/uploads/2022/10/best-logo-design-ideas-vector-file-download-now.jpg" alt=""
       className='mix-blend-multiply h-12 w-17 ' /></span>
-        <ul className='flex my-2 mx-3'>
+        <ul className='md:flex md:ml-8 my-2 mx-3 md:items-center'>
             <li className='mx-5 cursor-pointer
              hover:text-lg hover:text-yellow-100'>Home</li>
             <li className='mx-5 cursor-pointer
-            hover:text-lg hover:text-yellow-100'>Services</li>
+            hover:text-lg hover:text-yellow-100'>Belog's</li>
             <li className='mx-5 cursor-pointer
             hover:text-lg hover:text-yellow-100'>About</li>
             <li className='mx-5 cursor-pointer
