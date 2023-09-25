e000e88be45d8c2dfd91546e7f204d0a5f032f77
Asad45AsRa <143110934+Asad45AsRa@users.noreply.github.com>
2023-09-25 21:35:26 +0500

    web project

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './index.css'
 import Navbar from './Components/Navbar';
 
-function App() {
+export default function App() {
   return (
+    <>
     <Navbar />
+    </>
   )
 }
-
-export default App;
--- project/src/Components/Navbar.jsx
+++ project/src/Components/Navbar.jsx
@@ -1,30 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'F:/coding/Project/project/src/index.css'
+import { BiAlignRight } from "react-icons/bi";
 
 function Navbar() {
+
+    const [popup, setPopup] = useState(false)
+
+    const togglePopup = () => {
+        setPopup(!popup)
+    }
+
+    const [popup2, setPopup2] = useState(false)
+
+    const togglePopup2 = () => {
+        setPopup2(!popup2)
+    }
+
+
   return (
-    <nav className='shadow-md flex my-2 py-1 bg-slate-600 text-white font-medium justify-between font-[Poppins]
-    md:px-10 items-center'>
-      <span><img src="https://www.logodee.com/wp-content/uploads/2022/10/best-logo-design-ideas-vector-file-download-now.jpg" alt=""
-      className='mix-blend-multiply h-12 w-17 ' /></span>
-        <ul className='md:flex md:ml-8 my-2 mx-3 md:items-center'>
-            <li className='mx-5 cursor-pointer
-             hover:text-lg hover:text-yellow-100'>Home</li>
-            <li className='mx-5 cursor-pointer
-            hover:text-lg hover:text-yellow-100'>Belog's</li>
-            <li className='mx-5 cursor-pointer
-            hover:text-lg hover:text-yellow-100'>About</li>
-            <li className='mx-5 cursor-pointer
-            hover:text-lg hover:text-yellow-100'>Contact Us</li>
-            <button className='mx-5 cursor-pointer
-             hover:text-lg hover:text-yellow-100'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-           </svg>
-           </button>
-        </ul>
-        <span className='mr-8 App-header font-medium content-center pt-3'>Login</span>
-    </nav>
+    <div className='bg-gray-900 w-full text-white fixed py-1 my-1'>
+        <nav className='flex justify-between my-1 mx-1 font-Poppins center'>
+            <div className='font-semibold text- py-1'>
+                SecR<span className='text-amber-700'>adius</span>
+            </div>
+            <ul className='md:flex font-medium py-1 gap-10'>
+                <li><a href="/">Home</a></li>
+                <li
+                onMouseEnter={togglePopup}
+                onMouseLeave={togglePopup}
+                className='relative'><a href="/services">Services</a>
+                {popup && (
+                    <div className='absolute bg-slate-500 text-white p-4 mt-2 w-64 rounded-lg shadow-lg'>
+                        <p>Service 1</p>
+                        <p>Service 2</p>
+                        <p>Service 3</p>
+                        <p>Service 4</p>
+                    </div>
+                )}
+                </li>
+                <li
+                onMouseEnter={togglePopup2}
+                onMouseLeave={togglePopup2}
+                className='relative'><a href="/company">Company</a>
+                {popup2 && (
+                    <div className='absolute bg-slate-500 text-white p-4 mt-2 w-64 rounded-lg shadow-lg'>
+                        <p>Product 1</p>
+                        <p>Product 2</p>
+                        <p>Product 3</p>
+                        <p>Product 4</p>
+                    </div>
+                )}
+                </li>
+                <li><a href="/belogs">Belog's</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+            <div>
+                <button className='px-1 py-1 bg-red-500 rounded-md outline-none'>Login now</button>
+            </div>
+            <div>
+            <button className='font-medium text-xl py-1'><BiAlignRight /></button>
+            </div>
+        </nav>
+    </div>
   )
 }
 
-export default Navbar;
+export default Navbar
