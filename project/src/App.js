d188591e6ccca6e02b5a7d34d43bef4e772a698e
Asad45AsRa <143110934+Asad45AsRa@users.noreply.github.com>
2023-09-27 22:02:04 +0500

    web project

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import Navbar from './Components/Navbar';
 
 export default function App() {
   return (
     <>
-    <Navbar />
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
     </>
   )
 }
