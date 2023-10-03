2b6e08bfc1c4e67109aea9309d5d95ab7d8a9bbc
Asad45AsRa <143110934+Asad45AsRa@users.noreply.github.com>
2023-10-04 01:44:10 +0500

    web project

--- project/src/Components/About.jsx
+++ project/src/Components/About.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const About = () => {
+
+  const team = [
+    {Name : 'Haroon Hameed', AsA : 'CEO', image : './Assets/team/t-1.jpeg'},
+    {Name : 'Hanan Ahmed', AsA : 'CTO', image : './Assets/team/t-2.jpeg'},
+    {Name : 'Tanveer Ahmed', AsA : 'Sr Software Engineer', image : './Assets/team/t-3.jpeg'},
+    {Name : 'Javed Jamal', AsA : 'Lead Penetration Tester', image : './Assets/team/t-4.jpg'},
+    {Name : 'Zubair Ali', AsA : 'Security Engineer', image : './Assets/team/t-5.jpg'},
+    {Name : 'Usama Ijaz', AsA : 'Operational', image : './Assets/team/t-3.jpeg'},
+  ]
+
+  const [current, setCurrent] = useState(0)
+
+  const previousTeamFunction = () => {
+    if(current === 0) setCurrent(team.length -1);
+    else setCurrent(current - 1)
+  }
+
+  const nextTeamFunction = () => {
+    if(current === team.length - 1) setCurrent(0);
+    else setCurrent(current + 1)
+  }
+  
   return (
-    <div>About</div>
+    <>
+      <div className='justify-center text-white bg-slate-700 mt-10 py-7 items-center'>
+        <h1 className='text-5xl font-semibold items-center center px-96 mx-32'>About Us</h1>
+      </div>
+      <div className='flex justify-center pt-3 px-20 text-justify bg-slate-100 mix-blend-multiply border-amber-50 pb-4 pr-2 pl-2 rounded-md mt-10'>
+        <p className='font-serif text-gray-700 pt-20 px-4'>
+        At <strong>SecRadius</strong>, our priority is to keep our customers safe and secure from malicious hackers. 
+          We are a group of white-hat hackers & security experts who help companies to harden their 
+          security. Each web or mobile application has vulnerabilities as there is no patch for human 
+          mistakes. Keep running an application without a penetration test is no more than keeping the 
+          entire company's data and customers records at risk. We work together to provide a hack-proof 
+          security to our clients that will keep them safe from malicious hackers. This prevents the 
+          clients from a data breach and save them from being hacked. We simulate a hacker's approach 
+          in order to identify each and every security hole present in the target application, a final 
+          documentation is provided with a complete methodology to reproduce each vulnerability along 
+          with the patches which will make the security hack-proof. Don't be late, secure yourself 
+          before you get hacked.
+        </p>
+        <img src="./Assets/about/abt-2.jpg" alt=""className='mix-blend-multiply' />
+      </div>
+      <div className='items-center justify-center w-full h-auto px-5 py-10'>
+        <h1 className='mx-28 px-96 text-3xl font-serif font-bold'>Our Team</h1>
+        <div className='bg-slate-100 border-solid'>
+          Asad
+        </div>
+      </div>
+    </>
   )
 }
 
--- project/src/Components/Home.jsx
+++ project/src/Components/Home.jsx
@@ -1,30 +1,77 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, {useState} from 'react';
+import Typed from 'react-typed';
+import {BsArrowRight, BsArrowLeft} from 'react-icons/bs'
 
 const Home = () => {
 
+  const [current, setCurrent] = useState(0)
+
+  const slider = [
+    {id : 1, Text : 'Application \n Penetration Testing', image : './Assets/Slides/sl-1.png'},
+    {id : 2, Text : 'Cloud Security', image : './Assets/Slides/sl-2.png'},
+    {id : 3, Text : 'Volnerability \n Assessments', image : './Assets/Slides/sl-1.png'},
+    {id : 4, Text : 'Soc 2 Compliance', image : './Assets/Slides/sl-3.png'}
+  ]
+
   const backgroundContainer = {
-    backgroundImage: `url(${'./Assets/bg-5.jpg'})`,
+    backgroundImage: `url(${'./Assets/bg-7.jpg'})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
   }
+
+  const previousSliderFunction = () => {
+    if(current === 0) setCurrent(slider.length -1);
+    else setCurrent(current - 1)
+  }
+
+  const nextSliderFunction = () => {
+    if(current === slider.length - 1) setCurrent(0);
+    else setCurrent(current + 1)
+  }
+
+
+
   return (
     <>
-    
-    <div style={backgroundContainer} className='w-full h-screen opacity-2 shadow-md brightness-75 pt-10'>
-      <div className='text-green-500 max-w-[880px] mt-[-96px] w-full h-screen max-auto flex flex-col justify-center pl-96'>
-        <pre className='text-white
+      <div style={backgroundContainer} className='w-full h-screen opacity-2 shadow-md brightness-75 pt-10'>
+        <div className='text-green-500 max-w-[880px] mt-[-96px] w-full h-screen max-auto flex flex-col justify-center pl-96'>
+          <pre className='text-white
          text-5xl font-Poppins'>SHIELD YOUR </pre>
-        <h1 className='text-white text-5xl font-mono font-bold'>A G E N C Y</h1>
-        <p>Some text herte will b soon Now again</p>
-        <p>Some text herte will b soon Now again</p>
-        <div className='py-1 pl-48'>
-                <button className='py-1 px-1 rounded-md items-center outline-none bg-red-600 hover:bg-red-700 hover:text-yellow-200'>get Started</button>
-            </div>
+          <h1 className='text-white text-5xl font-mono font-bold'>A G E N C Y</h1>
+          <p>Some text herte will b soon Now again</p>
+          <p>Some text herte will b soon Now again</p>
+          <div>
+            <p>Hello Asad ul islam</p>
+            <Typed
+              strings={['Love', 'Soo', 'Much', 'Inshira']}
+              typeSpeed={120}
+              backSpeed={146}
+              loop
+            />
+          </div>
+          <div className='py-2'>
+            <button className='py-2 px-2 text-white font-semibold items-center outline-none bg-red-600 hover:bg-red-700 hover:py-2.5 hover:px2.5 text-hover:xl
+              hover:rounded-md duration-300'>GET STARTED</button>
+          </div>
+        </div>
+          <div className='text-white max-w-[880px] mt-[-90px] w-full h-16 max-auto flex justify-center pl-96'>
+           <div className='flex text-center items-center px-4 py-2'>
+           <p className='text-3xl font-bold font-Poppins uppercase cursor-pointer text-left'>{slider[current].Text}</p> <img src= {slider[current].image} alt="" className='
+           w-32 h-24 px-4 mix-blend-multiply rounded-sm'/>
+           </div>
+          </div>
+          <div className="flex justify-between text-white px-44 items-center text-center">
+        <button className='text-white bg-blue-500 hover:bg-blue-600 p-2 rounded-full'
+         onClick={previousSliderFunction}>
+          <BsArrowLeft />
+        </button>
+        <button className='text-white bg-blue-500 hover:bg-blue-600 p-2 rounded-full bottom-14'
+        onClick={nextSliderFunction}>
+          <BsArrowRight />
+        </button>
       </div>
-    </div>
-    <Outlet />
+      </div>
     </>
   )
 }
