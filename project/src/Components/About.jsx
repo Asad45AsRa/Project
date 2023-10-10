2aaf9d261edeee2030dcbd77f6cc64869561e00c
Asad45AsRa <143110934+Asad45AsRa@users.noreply.github.com>
2023-10-11 01:42:12 +0500

    web project

--- project/src/Components/About.jsx
+++ project/src/Components/About.jsx
@@ -7,11 +7,11 @@ const About = () => {
         <h1 className='text-5xl font-bold font-Hind pt-12'>About</h1>
         <h1 className='text-xl font-bold font-Hind pt-3'>Discover Our Story: Who We Are and What Drives Us</h1>
       </div>
-      <div className='max-w-[1250px] mx-auto my-10 p-2 md:grid grid-cols-2 duration-500'>
+      <div className='max-w-[1250px] mx-auto my-10 sm:pb-0 p-2 md:grid grid-cols-2 duration-500 md:border-b-2 bg-green-300'>
         <div className='col-span-1 md:w-[80%] text-center'>
           <img src="./Assets/about/abc.png" alt="" />
         </div>
-        <div className='col-span-1 flex flex-col justify-center text-justify p-2'>
+        <div className='col-span-1 flex flex-col justify-center text-justify p-2 bg-green-300'>
           <h1 className='text-bold text-lg font-bold font-Hind'>About Our Cybersecurity Team and Expertise</h1>
           <p className='font-Poppins2'>At <strong>SecRadius</strong>,we are a team of passionate cybersecurity 
           experts dedicated to safeguarding the digital world. With a mission to 
@@ -22,6 +22,75 @@ const About = () => {
           field ensure that your digital assets are fortified against emerging threats. 
           Get to know us better and learn how we can fortify your online presence</p>
         </div>
+      </div>
+      <div className='w-[100%] h-[100%] text-center items-center justify-center bg-green-300'>
+        <div className='sm:w-[97%] sm:h-[350px] md:h-[780px] sm:p-1 items-center justify-center text-center bg-green-300'>
+          <h1 className='sm:text-lg sm:font-bold font-Hind sm:p-1 md:text-3xl md:p-2'>Why SecRadius ?</h1>
+          <img src="./Assets/about/abc-2.png" alt="" className='md:mx-36 md:px-20 md:w-[69%] md:h-[69%]'/>
+        </div>
+        <div className='md:px-20 bg-green-300 p-2 pt-2'>
+          <h1 className='text-justify font-Hind text-2xl font-bold sm:py-1'>SecRadius: Defending Your Digital Realm</h1>
+          <ol className='text-justify list-disc font-semibold font-Poppins2'>
+          At SecRadius, our name encapsulates our core mission and values. Here's 
+          why 'SecRadius' is not just a name, but a reflection of our commitment 
+          to safeguarding your digital world:
+          <li className='pl-3 pt-2'>Security-Centric Approach: 'Sec' in SecRadius represents our unwavering focus on security. 
+            We are driven by a deep commitment to ensuring that your online assets are shielded from cyber 
+            threats and vulnerabilities, providing you with peace of mind in an increasingly interconnected 
+            digital landscape.</li>
+          <li className='pl-3 pt-2'>Expansive Protection: 'Radius' signifies our comprehensive approach to cybersecurity. We 
+            specialize in a diverse range of services, including Web Application Penetration Testing, 
+            API Penetration Testing, iOS and Android Apps Penetration Testing, Network Penetration Testing, 
+            and Source Code Review. This wide radius of expertise allows us to address every facet of your 
+            digital security needs.</li>
+          <li className='pl-3 pt-2'>Expertise and Excellence: With years of experience in the field, SecRadius brings a wealth of 
+            knowledge and proficiency to the table. Our commitment to excellence ensures that we go the extra 
+            mile to fortify your digital assets against emerging threats. Trust us to be your steadfast 
+            cybersecurity partner, and let's strengthen your online presence together</li>
+          </ol>
+        </div>
+      </div>
+      <div className='w-screen h-screen bg-green-300 pt-52'>
+        <div className='p-4 pt-8 text-center justify-center items-center'>
+        <h1 className='p-3 text-2xl font-Hind font-bold'>Certified Cybersecurity Excellence Our Credentials</h1>
+        <div className='flex mx-10 px-10 pt-3'>
+        <img src="./Assets/about/c-1.png" alt="" className='w-28 h-28 p-2'/>
+          <img src="./Assets/about/c-2.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-3.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-4.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-5.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-6.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-7.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-8.png" alt="" className='w-36 h-36 p-2' />
+        </div>
+        <div className='flex mx-14 px-14 pt-3'>
+          <img src="./Assets/about/c-9.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-10.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-11.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-12.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-13.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-14.png" alt="" className='w-36 h-36 p-2' />
+          <img src="./Assets/about/c-15.png" alt="" className='w-36 h-36 p-2' />
+        </div>
+        </div>
+      </div>
+      <div className='w-screen h-screen bg-green-300'>
+        <div className="text-center justify-center items-center">
+          <h1 className='text-5xl font-bold font-Hind'>Our Mission</h1>
+        </div>
+         <div className='max-w-[1250px] mx-auto my-10 sm:pb-0 p-2 md:grid grid-cols-2 duration-500 md:border-b-2 bg-green-300'>
+        <div className='col-span-1 md:w-[80%] md:h-[80%] text-center md:mt-8 md:pt-16'>
+          <div className='text-justify pt-10 font-bold font-Hind'>
+          At SecRadius, our mission is crystal clear: to fortify your digital world. 
+          We are driven by a relentless passion for cybersecurity and a commitment to safeguarding 
+          your online assets. With a team of dedicated experts and a diverse range of services, 
+          we stand as your trusted partner in protecting what matters most.
+          </div>
+        </div>
+        <div className='col-span-1 flex flex-col justify-center text-justify p-2 bg-green-300'>
+          <img src="./Assets/about/abc-3.png" alt="" />
+        </div>
+      </div>
       </div>
     </div>
   )
--- project/src/Components/Services.jsx
+++ project/src/Components/Services.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
-import Service from './Service'
 
 const Services = () => {
   return (
-    <>
-      <div className='w-full h-full text-center'>
-        <div style={{ backgroundImage: `url(${'./Assets/bg-10.jpg'})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}
-          className='w-full h-24 text-center justify-center text-black text-5xl my-3'>
-          <h1 className='font-sans font-bold text-center py-2'>Our Services</h1>
-        </div>
-        <div className='pt-16 px-6 pl-14'>
-          <div className='flex flex-wrap text-justify'>
-            <div className='flex-wrap w-96 px-11'>
-              <img src="./Assets/services/sr-1.png" alt="" className='w-36 h-36' />
-              <h1 className='font-bold text-xl px-2 py-1'>Web Application Penetration</h1>
-              <p>Web application penetration testing, also known as web
+    <div className='h-screen w-screen bg-green-300'>
+      <div className='w-full h-24 text-center items-center p-2'>
+        <h1 className='text-5xl font-Hind font-bold pt-5'>Services</h1>
+      </div>
+      <div className='col-span-1 md:w-[80%] text-center md:mx-8 md:px-10'>
+        <img src="./Assets/services/sr-36.png" alt="" className='inline h-60' />
+      </div>
+      <div className='col-span-1 flex flex-col justify-center p2 md:px-2 text-justify '>
+        <p className='text-xl font-Hind font-bold md:p-2'>
+          Discover the comprehensive suite of cybersecurity solutions offered by SecRadius.
+          From Web Application Penetration Testing to API Penetration Testing, iOS and
+          Android Apps Penetration Testing, Network Penetration Testing, and Source Code
+          Review, our expertise covers every aspect of digital security.
+        </p>
+        <p className='text-xl font-Hind font-bold sm:hidden md:block text-justify md:p-2'>
+          With a relentless commitment to excellence and a track record of success, we
+          provide tailored solutions to protect your digital assets. Explore our services
+          to learn how we can strengthen your defense against emerging cyber threats.
+        </p>
+        <p className='text-xl font-Hind font-bold sm:hidden md:block text-justify md:p-2'>At SecRadius, your security is our priority, and our services reflect that dedication.</p>
+      </div>
+      <div className='w-screen pt-10 h-screen bg-green-300'>
+        <div className='p-5 w-full h-full '>
+          <div className='grid-cols-3 flex p-2'>
+            <div className='col-span-1 p-3 border border-black rounded-lg mx-2'>
+              <div className='flex-wrap w-84 px-3 text-justify py-2'>
+                <img src="./Assets/services/sr-2.png" alt="" className='w-44 h-40' />
+                <h1 className='font-bold text-xl px-2 py-1'>Web Application Penetration</h1>
+                <p>Web application penetration testing, also known as web
                 app pentesting, is a security assessment aimed at
                 identifying vulnerabilities  within web-based applications.
                 Security experts use various techniques to simulate
@@ -22,66 +38,93 @@ const Services = () => {
                 cross-site scripting (XSS), and security misconfigurations.
                 The goal is to help organizations secure their web applications
                 and protect sensitive data.</p>
+                <div className='py-2'> 
+                  <button >Learn More</button>
+                </div>
+              </div>
             </div>
-            <div className='flex-wrap w-96 px-11'>
-              <img src="./Assets/services/sr-2.png" alt="" className='w-36 h-36' />
-              <h1 className='font-bold text-xl px-2 py-1'>API Penetration Testing</h1>
-              <p>API penetration testing focuses on the security of application
-                programming interfaces (APIs), which allow different software
-                components or services to communicate with each other. API
-                pentesters assess the authentication, authorization, and data
-                validation mechanisms of APIs to ensure they are protected against
-                unauthorized access and potential exploits.</p>
-            </div>
-            <div className='flex-wrap w-96 px-11'>
-              <img src="./Assets/services/sr-3.png" alt="" className='w-36 h-36' />
-              <h1 className='font-bold text-xl px-2 py-1'>IOS Apps Penetration</h1>
-              <p>iOS app penetration testing involves evaluating the security of iOS mobile 
-                applications. Penetration testers assess the app's code, server interactions, 
-                and data storage to identify vulnerabilities that could be exploited by 
-                attackers. The goal is to ensure that iOS apps are resilient to attacks 
-                and protect user data.</p>
-            </div>
-            <div className='flex-wrap w-96 px-11'>
-              <img src="./Assets/services/sr-5.png" alt="" className='w-36 h-36' />
-              <h1 className='font-bold text-xl px-2 py-1'>Network Penetration</h1>
-              <p>Network penetration testing, often referred to as network pentesting or 
+            <div className='col-span-1 p-3 border border-black rounded-lg mx-2'>
+              <div className='flex-wrap w-84 px-3 text-justify py-2'>
+                <img src="./Assets/services/sr-6.png" alt="" className='w-44 h-40' />
+                <h1 className='font-bold text-xl px-2 py-1'>Network Penetration</h1>
+                <p>Network penetration testing, often referred to as network pentesting or 
                 ethical hacking, involves assessing the security of an organization's 
                 network infrastructure. Pentesters identify vulnerabilities in routers, 
                 firewalls, switches, and other network components. They aim to find 
                 weaknesses that could be exploited to gain unauthorized access or 
                 disrupt network operations.</p>
+                <div className='py-2'> 
+                  <button >Learn More</button>
+                </div>
+              </div>
             </div>
-            <div className='flex-wrap w-96 px-11'>
-              <img src="./Assets/services/sr-4.png" alt="" className='w-36 h-36' />
-              <h1 className='font-bold text-xl px-2 py-1'>Andriod Apps Penetration</h1>
-              <p>Similar to iOS app penetration testing, this service focuses on 
-                evaluating the security of Android mobile applications. Penetration 
-                testers examine the app's code, data storage, and communication 
-                protocols to uncover vulnerabilities that could be exploited. The 
-                goal is to enhance the security of Android apps and protect user 
-                information.</p>
+            <div className='col-span-1 p-3 border border-black rounded-lg mx-2'>
+              <div className='flex-wrap w-84 px-3 text-justify py-2'>
+                <img src="./Assets/services/sr-3.png" alt="" className='w-44 h-40' />
+                <h1 className='font-bold text-xl px-2 py-1'>API Penetration Testing</h1>
+                <p>API penetration testing focuses on the security of application
+                programming interfaces (APIs), which allow different software
+                components or services to communicate with each other. API
+                pentesters assess the authentication, authorization, and data
+                validation mechanisms of APIs to ensure they are protected against
+                unauthorized access and potential exploits.</p>
+                <div className='py-2'> 
+                  <button >Learn More</button>
+                </div>
+              </div>
             </div>
-            <div className='flex-wrap w-96 px-11'>
-              <img src="./Assets/services/sr-6.png" alt="" className='w-36 h-36' />
-              <h1 className='font-bold text-xl px-2 py-1'>Source Code Review</h1>
-              <p>Source code review, also known as code auditing or static analysis, 
+            
+          </div>
+          <div className='grid-cols-3 flex p-2'>
+            <div className='col-span-1 p-3 border border-black rounded-lg mx-2'>
+              <div className='flex-wrap w-84 px-3 text-justify py-2'>
+                <img src="./Assets/services/sr-1.png" alt="" className='w-44 h-40' />
+                <h1 className='font-bold text-xl px-2 py-1'>IOS Apps Penetration</h1>
+                <p>iOS app penetration testing involves evaluating the security of iOS mobile 
+                applications. Penetration testers assess the app's code, server interactions, 
+                and data storage to identify vulnerabilities that could be exploited by 
+                attackers. The goal is to ensure that iOS apps are resilient to attacks 
+                and protect user data.</p>
+                <div className='py-2'> 
+                  <button >Learn More</button>
+                </div>
+              </div>
+            </div>
+            <div className='col-span-1 p-3 border border-black rounded-lg mx-2'>
+              <div className='flex-wrap w-84 px-3 text-justify py-2'>
+                <img src="./Assets/services/sr-5.png" alt="" className='w-44 h-40' />
+                <h1 className='font-bold text-xl px-2 py-1'>Source Code Review</h1>
+                <p>Source code review, also known as code auditing or static analysis, 
                 involves a thorough examination of an application's source code to 
                 identify security flaws and coding errors. Security experts analyze 
                 the codebase to find vulnerabilities like buffer overflows, injection 
                 flaws, and insecure authentication mechanisms. The primary objective is 
                 to eliminate coding vulnerabilities before they can be exploited.</p>
+                <div className='py-2'> 
+                  <button >Learn More</button>
+                </div>
+              </div>
             </div>
-
+            <div className='col-span-1 p-3 border border-black rounded-lg mx-2'>
+              <div className='flex-wrap w-84 px-3 text-justify py-2'>
+                <img src="./Assets/services/sr-4.png" alt="" className='w-44 h-40' />
+                <h1 className='font-bold text-xl px-2 py-1'>Andriod Apps Penetration</h1>
+                <p>Similar to iOS app penetration testing, this service focuses on 
+                evaluating the security of Android mobile applications. Penetration 
+                testers examine the app's code, data storage, and communication 
+                protocols to uncover vulnerabilities that could be exploited. The 
+                goal is to enhance the security of Android apps and protect user 
+                information.</p>
+                <div className='py-2'> 
+                  <button >Learn More</button>
+                </div>
+              </div>
+            </div>
+            
           </div>
         </div>
-        <div className='w-full my-6 h-96'
-        style={{backgroundImage : `url(${'./Assets/bg-11.jpg'})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
-          
-        </div>
       </div>
-      <Service />
-    </>
+    </div>
   )
 }
 
